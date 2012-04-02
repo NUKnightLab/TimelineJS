0df921d035ba00042d1de6c63f2243c641498853
Zach Wise <zach@digitalartwork.net>
2012-04-01 23:19:38 -0500

    Fix for IE problem loading from google docs thnx @mwidmann

--- timeline.js
+++ timeline.js
@@ -5450,12 +5450,21 @@ if(typeof VMM != 'undefined' && typeof VMM.Timeline == 'undefined') {
 			type: "google spreadsheet",
 			
 			getData: function(raw_data) {
-				//https://spreadsheets.google.com/feeds/list/0Agl_Dv6iEbDadGRwZjJSRTR4RHJpanE2U3lkb0lyYUE/od6/public/values?alt=json
-				// https://docs.google.com/a/digitalartwork.net/spreadsheet/ccc?hl=en_US&key=0Agl_Dv6iEbDadGRwZjJSRTR4RHJpanE2U3lkb0lyYUE&rm=full#gid=0
 				var _key = VMM.Util.getUrlVars(raw_data)["key"];
 				var _url = "https://spreadsheets.google.com/feeds/list/" + _key + "/od6/public/values?alt=json";
-				
-				VMM.getJSON(_url, VMM.Timeline.DataObj.model_GoogleSpreadsheet.buildData);
+
+				if ( $.browser.msie && parseInt($.browser.version, 10) >= 8 && window.XDomainRequest) {
+					// Use Microsoft XDR
+					var xdr = new XDomainRequest();
+					xdr.open("get", _url);
+					xdr.onload = function() {
+						var json = $.parseJSON( xdr.responseText );
+						VMM.Timeline.DataObj.model_GoogleSpreadsheet.buildData( json );
+					}
+					xdr.send();
+				} else {
+					VMM.getJSON(_url, VMM.Timeline.DataObj.model_GoogleSpreadsheet.buildData);
+				}
 				
 			},
 			
