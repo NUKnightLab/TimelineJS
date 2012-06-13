d430dc92b40a32aa44e8915a1ed07e562afec9ed
Zach Wise <zach@digitalartwork.net>
2012-06-13 15:47:58 -0500

    Fix for weird GitHub merge

--- source/js/locale/ru.js
+++ source/js/locale/ru.js
@@ -1,7 +1,6 @@
 /* LANGUAGE 
  ================================================== */
 if (typeof VMM != "undefined") {
-<<<<<<< HEAD
 	VMM.Language = {
 		lang:"ru",
 		api:{
@@ -34,37 +33,4 @@ if (typeof VMM != "undefined") {
 			loading: "Загрузка"
 		}
 	}
-=======
-    VMM.debug = !1;
-    VMM.Language = {
-        lang:"ru",
-        api:{
-            wikipedia:"ru"
-        },
-        date:{
-            month:["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
-            month_abbr:["Янв.", "Фев.", "Март", "Апр.", "Май", "Июнь", "Июль", "Авг.", "Сент.", "Окт.", "Нояб.", "Дек."],
-            day:["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
-            day_abbr:["Вск.", "Пн.", "Вт.", "Ср.", "Чт.", "Пт.", "Сб."]
-        },
-        dateformats:{
-            year:"yyyy",
-            month_short:"mmm",
-            month:"mmmm yyyy",
-            full_short:"mmm d",
-            full:"mmmm d',' yyyy",
-            time_no_seconds_short:"h:MM TT",
-            time_no_seconds_small_date:"h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
-            full_long:"mmm d',' yyyy 'at' hh:MM TT",
-            full_long_small_date:"hh:MM TT'<br/><small>mmm d',' yyyy'</small>'"
-        },
-        messages:{
-            loading_timeline:"Загрузка... ",
-            return_to_title:"Вернуться к заголовку",
-            expand_timeline:"Увеличить",
-            contract_timeline:"Уменьшить",
-            wikipedia:"Из Wikipedia",
-            loading_content:"Загрузка контента"}
-    }
->>>>>>> 1588025872befb364ff5e22dcd0c159302b3b669
 };
