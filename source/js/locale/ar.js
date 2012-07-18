b3205111a9e56914f74cc1f674c93e7618a84011
Zach Wise <zach@digitalartwork.net>
2012-07-18 15:18:51 -0500

    Arabic language added but it looks like the characters got mangled

---
+++ source/js/locale/ar.js
@@ -0,0 +1,36 @@
+/* LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "ar",
+		api: {
+			wikipedia: "ar"
+		},
+		date: {
+			month: ["ﬂ«‰Ê‰ «·À«‰Ì", "‘»«ÿ", "¬–«—", "‰Ì”«‰", "√Ì«—", "Õ“Ì—«‰", " „Ê“", "¬»", "√Ì·Ê·", " ‘—Ì‰ «·√Ê·", " ‘—Ì‰ «·À«‰Ì", "ﬂ«‰Ê‰ «·√Ê·"],
+			month_abbr: ["ﬂ«‰Ê‰ «·À«‰Ì", "‘»«ÿ", "¬–«—", "‰Ì”«‰", "√Ì«—", "Õ“Ì—«‰", " „Ê“", "¬»", "√Ì·Ê·", " ‘—Ì‰ «·√Ê·", " ‘—Ì‰ «·À«‰Ì", "ﬂ«‰Ê‰ «·√Ê·"],
+			day: ["«·√Õœ","«·«À‰Ì‰", "«·À·«À«¡", "«·√—»⁄«¡", "«·Œ„Ì”", "«·Ã„⁄…", "«·”» "],
+			day_abbr: ["«·√Õœ","«·«À‰Ì‰", "«·À·«À«¡", "«·√—»⁄«¡", "«·Œ„Ì”", "«·Ã„⁄…", "«·”» "]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "mmm d",
+			full: "mmmm d',' yyyy",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+			full_long: "mmm d',' yyyy 'at' hh:MM TT",
+			full_long_small_date: "hh:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Ã«—Ì «· Õ„Ì·... ",
+			return_to_title: "«·⁄Êœ…",
+			expand_timeline: " ﬂ»Ì— «·⁄—÷",
+			contract_timeline: "«·« ›«ﬁÌ…",
+			wikipedia: "„‰ ÊÌﬂÌ»ÌœÌ«, «·„Ê”Ê⁄… «·Õ—…",
+			loading_content: " Õ„Ì· «·„Õ ÊÏ",
+			loading: " Õ„Ì·"
+		}
+	}
+}
