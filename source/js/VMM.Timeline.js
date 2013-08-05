7e1080b427191d173b1b4e77b4f7f2e7fdd8f01d
Joe Germuska <joe@germuska.com>
2013-08-05 13:09:59 -0500

    add NUKL to banner

--- source/js/VMM.Timeline.js
+++ source/js/VMM.Timeline.js
@@ -1,6 +1,6 @@
 /**
 	* TimelineJS
-	* Designed and built by Zach Wise at VéritéCo
+	* Designed and built by Zach Wise for the Northwestern University Knight Lab
 
 	* This Source Code Form is subject to the terms of the Mozilla Public
 	* License, v. 2.0. If a copy of the MPL was not distributed with this
