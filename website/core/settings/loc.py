13774b20f18d23dfb69c65dd151e3aed9734a88f
jywsn <jennifer.y.wilson@northwestern.edu>
2014-03-17 13:26:50 -0500

    Comment out secrets import (not needed for this project)

--- website/core/settings/loc.py
+++ website/core/settings/loc.py
@@ -3,12 +3,12 @@ import sys
 from os.path import normpath, join
 from .base import *
 
-# Import secrets
-sys.path.append(
-    abspath(join(PROJECT_ROOT, '../secrets/buzz/stg'))
-)
+# Import secrets -- not needed
+#sys.path.append(
+#    abspath(join(PROJECT_ROOT, '../secrets/TimelineJS/stg'))
+#)
 
-from secrets import *
+#from secrets import *
 
 # Set static URL
 STATIC_URL = '/static'
