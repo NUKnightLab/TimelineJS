607deb090fc907f5895b250ab42bbd86e622db46
Joe Germuska <joe@germuska.com>
2014-05-06 08:08:38 -0500

    Updates to Hungarian l10n. Closes NUKnightLab/StoryJS-Core#42 Thanks @mtorok

--- source/js/Core/Language/locale/hu.js
+++ source/js/Core/Language/locale/hu.js
@@ -8,21 +8,21 @@ if(typeof VMM != 'undefined') {
 		},
 		date: {
 			month: ["január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"],
-			month_abbr: ["jan.", "febr.", "márc", "ápr.", "máj.", "jún.", "júl.", "aug.", "szept.", "okt.", "nov.", "dec."],
+			month_abbr: ["jan.", "febr.", "márc.", "ápr.", "máj.", "jún.", "júl.", "aug.", "szept.", "okt.", "nov.", "dec."],
 			day: ["vasárnap","hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat"],
 			day_abbr: ["vas.","hétfő", "kedd", "szer.", "csüt.", "pén.", "szom."]
 		}, 
 		dateformats: {
 			year: "yyyy",
 			month_short: "mmm",
-			month: "yyyy mmmm",
-			full_short: "mmm d",
-			full: "yyyy mmmm d",
+			month: "yyyy. mmmm",
+			full_short: "mmm d.",
+			full: "yyyy. mmmm d.",
 			time_short: "HH:MM:SS",
 			time_no_seconds_short: "HH:MM",
-			time_no_seconds_small_date: "HH:MM '<br/><small>'yyyy mmmm d'</small>'",
-			full_long: "yyyy mmm d',' HH:MM",
-			full_long_small_date: "HH:MM '<br/><small>yyyy mmm d'</small>'"
+			time_no_seconds_small_date: "HH:MM '<br/><small>'yyyy. mmmm d.'</small>'",
+			full_long: "yyyy. mmm d.',' HH:MM",
+			full_long_small_date: "HH:MM '<br/><small>yyyy. mmm d.'</small>'"
 		},
 		messages: {
 			loading_timeline: "Az idővonal betöltése... ",
