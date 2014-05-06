3e760a5a1f3877758e659c0cb7e907ce551dfac1
Joe Germuska <joe@germuska.com>
2014-05-06 08:03:15 -0500

    update Italian translation. Closes NUKnightLab/StoryJS-Core#46 thanks @lollando

--- source/js/Core/Language/locale/it.js
+++ source/js/Core/Language/locale/it.js
@@ -9,7 +9,7 @@ if(typeof VMM != 'undefined') {
 		date: {
 			month: ["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"],
 			month_abbr: ["Gen","Feb","Mar","Apr","Mag","Giu","Lug","Ago","Set","Ott","Nov","Dic"],
-			day: ["Domenica","Lunedi","Martedi","Mercoledi","Giovedi","Venerdi","Sabato"],
+			day: ["Domenica","Lunedí","Martedí","Mercoledí","Giovedí","Venerdí","Sabato"],
 			day_abbr: ["Dom.","Lun.","Mar.","Mer.","Gio.","Ven.","Sab."]
 		}, 
 		dateformats: {
@@ -21,12 +21,12 @@ if(typeof VMM != 'undefined') {
 			time_short: "HH:MM:ss",
 			time_no_seconds_short: "HH:MM",
 			time_no_seconds_small_date: "HH:MM'<br/><small>'d mmmm yyyy'</small>'",
-			full_long: "dddd',' d mmm yyyy 'um' HH:MM",
+			full_long: "dddd',' d mmm yyyy 'alle' HH:MM",
 			full_long_small_date: "HH:MM'<br/><small>'dddd',' d mmm yyyy'</small>'"
 		},
 		messages: {
 			loading_timeline: "Caricamento Timeline... ",
-			return_to_title: "Ritorna al Titolo",
+			return_to_title: "Ritorna all'inizio",
 			expand_timeline: "Espandi la Timeline",
 			contract_timeline: "Contrai la Timeline",
 			wikipedia: "Wikipedia, L’enciclopedia libera",
