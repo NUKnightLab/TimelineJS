b74c74f40182730e54f255c0b58de9ed13762690
Joe Germuska <joe@germuska.com>
2014-07-31 14:45:00 -0500

    integrate changes from git issue. closes #568

--- source/js/Core/Language/locale/ko.js
+++ source/js/Core/Language/locale/ko.js
@@ -1,23 +1,23 @@
 /* Korean LANGUAGE 
 ================================================== */
-if(typeof VMM != 'undefined') {
+if (typeof VMM != 'undefined') {
 	VMM.Language = {
 		lang: "ko",
 		api: {
 			wikipedia: "ko"
 		},
 		date: {
-			month: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
-			month_abbr: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
-			day: ["일요일" , "월요일" , "화요일" , "수요일" , "목요일" , "금요일" , "토요일"],
-			day_abbr: ["일" , "월" , "화" , "수" , "목" , "금" , "토"]
-		}, 
+			month: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+			month_abbr: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+			day: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+			day_abbr: ["일", "월", "화", "수", "목", "금", "토"]
+		},
 		dateformats: {
 			year: "yyyy",
-			month_short: "mmm",
-			month: "yyyy mmm",
-			full_short: "mmm d",
-			full: "yyyy mmm d ",
+			month_short: "mm",
+			month: "yyyy년 m월",
+			full_short: "mm-dd",
+			full: "yyyy년 m월 d일 ",
 			time_short: "HH:MM:ss",
 			time_no_seconds_short: "HH:MM",
 			time_no_seconds_small_date: "HH:MM'<br/><small>'yyyy mmm d'</small>'",
@@ -32,7 +32,6 @@ if(typeof VMM != 'undefined') {
 			wikipedia: "출처: 위키피디아, 우리 모두의 백과사전",
 			loading_content: "내용을 불러오고 있습니다.",
 			loading: "불러오는중"
-			
 		}
 	}
 }
