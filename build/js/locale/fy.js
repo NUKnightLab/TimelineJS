a537b75396a4b4ff1357a4d97fd31ab9b79910ce
Fokko Driesprong <fokko@driesprongen.nl>
2014-10-05 20:21:06 +0200

    Added the Frisian language.

---
+++ build/js/locale/fy.js
@@ -0,0 +1,37 @@
+/* English LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "fy",
+		api: {
+			wikipedia: "fy"
+		},
+		date: {
+			month: ["Foarmoanne", "Sellemoanne", "Foarjiersmoanne", "Gersmoanne", "Blommemoanne", "Simmermoanne", "Heamoanne", "Rispmoanne", "Hjerstmoanne", "Wynmoanne", "Slachtmoanne", "Wintermoanne"],
+			month_abbr: ["Jan.", "Feb.", "Mar", "Apr", "Mei", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Des."],
+			day: ["Snein","Moandei", "Tiisdei", "Woansdei", "Tongersdei", "Freed", "Sneon"],
+			day_abbr: ["Snein","Moandei", "Tiisdei", "Woansdei", "Tongersdei", "Freed", "Sneon"]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "d mmm",
+			full: "d mmmm yyyy",
+			time_short: "HH:MM:ss",
+			time_no_seconds_short: "HH:MM",
+			time_no_seconds_small_date: "HH:MM'<br/><small>'d mmmm yyyy'</small>'",
+			full_long: "dddd',' d mmm yyyy 'om' HH:MM",
+			full_long_small_date: "HH:MM'<br/><small>'dddd',' d mmm yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Tiidline ynlade ... ",
+			return_to_title: "Wer werom nei it begjin",
+			expand_timeline: "Tiidline útzoomen",
+			contract_timeline: "Tiidline ynzoomen",
+			wikipedia: "Fan Wikipedia, de frije ensyklopedy",
+			loading_content: "Ynhâld ynlade",
+			loading: "Ynlade"
+		}
+	}
+}
