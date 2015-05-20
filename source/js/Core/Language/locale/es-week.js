8099bf6b818c30e87677292d6dd3979ac7bf170b
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-05-20 08:36:27 -0430

    add: es-week.js translation from en-week.js

---
+++ source/js/Core/Language/locale/es-week.js
@@ -0,0 +1,38 @@
+/* Spanish LANGUAGE
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "es",
+		api: {
+			wikipedia: "es"
+		},
+		date: {
+			month: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+			month_abbr: ["Ene.", "Feb.", "Mar.", "Abr.", "May.", "Jun.", "Jul.", "Ago.", "Sep.", "Oct.", "Nov.", "Dic."],
+			day: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+			day_abbr: ["Dom.", "Lun.", "Mar.", "Mié.", "Jue.", "Vie.", "Sáb."]
+		},
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm 'de' yyyy",
+			full_short: "'Semana' W",
+			full: "'Semana' W",
+			time_short: "HH:MM:ss",
+			time_no_seconds_short: "HH:MM TT",
+			time_no_seconds_small_date: "HH:MM TT'<br/><small>'d 'de' mmmm 'de' yyyy'</small>'",
+			full_long: "d 'de' mmm 'de' yyyy 'a las' HH:MM TT",
+			full_long_small_date: "HH:MM TT'<br/><small>d 'de' mmm',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Cargando la Cronología... ",
+			return_to_title: "Volver al Título",
+			expand_timeline: "Expandir la Cronología",
+			contract_timeline: "Reducir la cronología",
+			wikipedia: "Desde Wikipedia, la enciclopedia libre",
+			loading_content: "Cargando Contenido",
+			loading: "Cargando",
+			swipe_nav: "Pasar a Navegación"
+		}
+	}
+}
